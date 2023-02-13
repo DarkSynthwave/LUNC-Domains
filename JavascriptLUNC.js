2f906c3c1c94dd4b433707631af8e6cb32e2d716
DarkSynthwave <125248947+DarkSynthwave@users.noreply.github.com>
2023-02-13 18:44:39 +1100

    Update JavascriptLUNC.js

--- JavascriptLUNC.js
+++ JavascriptLUNC.js
@@ -1,5 +1,5 @@
 <script>
-<script type="text/javascript" src="JavascriptLUNC.js"></script>
+<script type="text/javascript" src="DarkSynthwave/LUNC-Domains/JavascriptLUNC.js"></script>
   const form = document.querySelector("form");
   const domainNameInput = document.querySelector("#domain-name");
   const bidInput = document.querySelector("#bid");
